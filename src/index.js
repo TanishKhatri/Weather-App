4389d49f3f7054829b518b4ee4e6abca22941514
Tanish Khatri <tanishkhatriofficial@gmail.com>
2026-01-06 11:47:37 +0000

    Weather Icons are added, as well as test of VC

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-import './styles.css';
+import "./styles.css";
 console.log("It Works");
+
+async function getWeatherObject(chosenCity) {
+  try {
+    const currWeatherResponse = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${chosenCity}?key=JGPVJ3TGWWGEJ3LPNP2H2CKEM`
+    );
+    const currWeatherObject = await currWeatherResponse.json();
+    return currWeatherObject;
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+getWeatherObject("delhi").then((result) => {console.log(result)});
